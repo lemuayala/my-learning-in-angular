a560fb52754a70aba54cf9bad8516385edd1b0b3
lemuayala <lemuayala@gmail.com>
2024-06-10 19:18:00 -0300

    Add Bootstrap styles to the template, pagination for the task list, and some ngClass directives

--- my-learning-in-angular/src/app/task-list/task-list.component.ts
+++ my-learning-in-angular/src/app/task-list/task-list.component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import { TaskService } from '../services/task.service';
 import { CommonModule, NgFor } from '@angular/common';
 import { RouterLink, RouterModule } from '@angular/router';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule, NgxPaginationModule],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css',
 })
 export class TaskListComponent {
   tasks!: any[];
+  currentPage: number = 1;
 
   constructor(private taskService: TaskService) {
     this.getTask();
